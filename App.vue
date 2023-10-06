5d0c2943cbacc9c33dddbf53aa7712fca526c05a
liammcknightt <113410889+liammcknightt@users.noreply.github.com>
2023-10-06 18:39:37 +1100

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/designsandtasks">Designs and Tasks</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link>
+  </div>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
