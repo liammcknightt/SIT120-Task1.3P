6a6385f1a7251a645e9ec5019dab60aa3061b540
liammcknightt <113410889+liammcknightt@users.noreply.github.com>
2023-10-06 18:38:32 +1100

    Create HomeView.vue
    Home View

---
+++ HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+      <h1 style="font-size:60px; text-align:center; margin:5px">SIT120 - Introduction to Responsive Web Apps</h1>
+      <h2 style="font-size:30px; text-align:center; margin:15px;">Liam McKnight - Portfolio</h2>
+  </div>
+  <div class="row">
+  <div class="column">
+    <img src="@/assets/1.png" alt="Task 1.1P" style="width:100%">
+  </div>
+  <div class="column">
+    <img src="@/assets/2.png" alt="Task 1.2P" style="width:100%">
+  </div>
+  </div>
+</template>
